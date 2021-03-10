0042b68fd68434e022a0e4b320539946bb707165
LucasFuentesValenzuela <lucas.fuentes-valenzuela@outlook.com>
2021-03-09 17:03:45 -0800

    fixing labels

--- predictors.py
+++ predictors.py
@@ -159,9 +159,9 @@ class MADpredictor(torch.nn.Module):
 
         # label
         if node_type == 'source':
-            label = self.uncertainty * (self.adj[samples, nodes_.unsqueeze(0).unsqueeze(2)] * 2 - 1)
+            label = self.uncertainty * (self.adj[samples, tgt_nodes.unsqueeze(0).unsqueeze(2)] * 2 - 1)
         elif node_type == 'target':
-            label = self.uncertainty * (self.adj[nodes_.unsqueeze(0).unsqueeze(2), samples] * 2 - 1)
+            label = self.uncertainty * (self.adj[src_nodes.unsqueeze(0).unsqueeze(2), samples] * 2 - 1)
 
 
         # logits should be shape (n_heads, n_batch, n_samples)
